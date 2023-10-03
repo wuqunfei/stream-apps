3817e95311d9dda718d659f08a7a5e663cbc2334
Qunfei Wu <wu.qunfei@gmail.com>
2023-10-03 23:58:33 +0200

    Adding All Stream Function into Cloud Application

--- word-counter-spring-cloud-stream-kafka/src/main/java/org/github/stream/lesson/StreamsCloudApplication.java
+++ word-counter-spring-cloud-stream-kafka/src/main/java/org/github/stream/lesson/StreamsCloudApplication.java
@@ -1,9 +1,12 @@
 package org.github.stream.lesson;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.context.annotation.Bean;
+import java.util.Arrays;
+import java.util.function.Function;
 
 @SpringBootApplication
 public class StreamsCloudApplication {
@@ -11,5 +14,21 @@ public class StreamsCloudApplication {
     public static void main(String[] args) {
         SpringApplication.run(StreamsCloudApplication.class, args);
     }
+
+    @Bean
+    public Function<KStream<String, String>, KStream<String, Long>> wordProcess() {
+        return StreamsCloudApplication::apply;
+    }
+
+    private static KStream<String, Long> apply(KStream<String, String> stream) {
+
+        KTable<String, Long> counterTable = stream.flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+                .groupBy((key, word) -> word, Grouped.with(Serdes.String(), Serdes.String()))
+                .count(Materialized.as("counter-store"));
+        KStream<String, Long> outputStream = counterTable.toStream();
+        outputStream.to("output-topic", Produced.with(Serdes.String(), Serdes.Long()));
+        return outputStream;
+    }
+
 
 }
